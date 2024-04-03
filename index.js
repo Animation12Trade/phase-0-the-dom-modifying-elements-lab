1fcc917f4936e6e942db78daec8502f76d2c481b
+ <omuhindaaaron@gmail.com>
2024-04-03 21:38:29 +0300

    commit DOM

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const body1 = document.getElementById("main");
+if(body1){
+    body1.parentNode.removeChild(body1);
+}
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id","victory")
+
+newHeader.textContent= "YOUR-NAME is the champion";
+
+document.body.appendChild(newHeader)
+
